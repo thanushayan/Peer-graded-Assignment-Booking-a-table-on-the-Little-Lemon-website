598a28b50cd8d1c0d2bb4666951a78d134225b96
thanushayan <thanushayan17@gmail.com>
2023-12-25 21:31:55 +0530

    first commit

---
+++ src/components/Menu.js
@@ -0,0 +1,63 @@
+import React from "react";
+  // Assuming recipes.js is in the same directory as Menu.js
+import Swal from "sweetalert2";
+import recipes from '../data/recipes';
+
+
+const Menu = () => {
+//   console.log(recipes[0]);
+const handleOrder = (id) => {
+    console.log(id);
+
+    const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+          confirmButton: 'btn btn-success',
+          cancelButton: 'btn btn-danger'
+        },
+        buttonsStyling: false
+      })
+      
+      swalWithBootstrapButtons.fire({
+        title: 'Do you want to confirm order?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: false,
+        confirmButtonText: 'Yes, order it!',
+        reverseButtons: true
+      }).then((result) => {
+        if (result.isConfirmed) {
+          swalWithBootstrapButtons.fire(
+            'Orderd!',
+            'Your order has been confirmed.',
+            'success'
+          )
+        } 
+      })
+
+}
+  return (
+    <div className="menu-container">
+      <div className="menu-header">
+        <h2>This weeks specials!</h2>
+        <button>Online Menu</button>
+      </div>
+      <div className="cards">
+        {recipes.map((recipe) => (
+          <div key={recipe.id} className="menu-items">
+            <img src={recipe.image} alt="" />
+            <div className="menu-content">
+              <div className="heading">
+                <h5>{recipe.title}</h5>
+                <p>${recipe.price}</p>
+              </div>
+              <p>{recipe.description}</p>
+              <button className="orderbtn" onClick={() => handleOrder(recipe.id)}>Order Now</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
